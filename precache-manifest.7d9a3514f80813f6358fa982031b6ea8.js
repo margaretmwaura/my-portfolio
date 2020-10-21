28d20f95ec3d557656bb4290100de5a16f716e86
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-10-21 17:11:28 +0300

    sorting issues on the favicon and the title text

---
+++ precache-manifest.7d9a3514f80813f6358fa982031b6ea8.js
@@ -0,0 +1,5 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "main.11b39429e491ad2e9eb6.js"
+  }
+]);
---
+++ precache-manifest.ad24c8379a634faac34d4756fd2948c5.js
@@ -0,0 +1,5 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "main.cf634dfd83b536376a38.js"
+  }
+]);
