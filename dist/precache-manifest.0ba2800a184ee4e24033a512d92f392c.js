b1cdac96a3172ed66f2bae0820d65b1042b1be84
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-10-21 19:15:51 +0300

    removing the what input script

---
+++ dist/precache-manifest.0ba2800a184ee4e24033a512d92f392c.js
@@ -0,0 +1,5 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "main.962faa60d51ba55d226c.js"
+  }
+]);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import 'script-loader!../foundation/js/vendor/jquery';
 import 'script-loader!foundation-sites/dist/js/foundation.min';
-import 'script-loader!../foundation/js/vendor/what-input';
 
 import Vue from 'vue'
 import App from './App.vue'
