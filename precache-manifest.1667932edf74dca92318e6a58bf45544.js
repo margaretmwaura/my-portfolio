ecc4c44106e95d769e88d89ca63d727a52643231
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-10-22 20:06:19 +0300

    adding dist subtree

--- precache-manifest.1667932edf74dca92318e6a58bf45544.js
+++
@@ -1,8 +0,0 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "url": "client.91305a985e4427623549.js"
-  },
-  {
-    "url": "vendor.91305a985e4427623549.js"
-  }
-]);
---
+++ precache-manifest.4c444a99165d9df94b1f845b7f571ec8.js
@@ -0,0 +1,8 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "client.ed6fa4c0339312143058.js"
+  },
+  {
+    "url": "vendor.ed6fa4c0339312143058.js"
+  }
+]);
