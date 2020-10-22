c26365c628a259bae1666aa7e51f9c10c448ec68
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-10-22 09:45:22 +0300

    diving the imports into client and vendor

---
+++ precache-manifest.86693696039e8988ba8f5410395e265c.js
@@ -0,0 +1,8 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "client.7aef98c7a2dbcd4e5980.js"
+  },
+  {
+    "url": "vendor.7aef98c7a2dbcd4e5980.js"
+  }
+]);
---
+++ precache-manifest.8a9e1ded1167a146125dfbf1d38409ec.js
@@ -0,0 +1,8 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "client.f5b524af9bf3329dbce4.js"
+  },
+  {
+    "url": "vendor.f5b524af9bf3329dbce4.js"
+  }
+]);
