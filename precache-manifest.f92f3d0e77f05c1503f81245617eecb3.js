1b98c3589afb2114c5b19eb87143bd06ecb85fc3
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-10-22 19:29:13 +0300

    adding the projects section

---
+++ precache-manifest.f92f3d0e77f05c1503f81245617eecb3.js
@@ -0,0 +1,8 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "client.e44322f61b93569870e7.js"
+  },
+  {
+    "url": "vendor.e44322f61b93569870e7.js"
+  }
+]);
