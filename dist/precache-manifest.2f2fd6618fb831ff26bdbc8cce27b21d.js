a08a941d7eb4d2e1e30528a8946db72ed43a1367
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-12-02 04:40:10 +0300

    adding dist subtree

---
+++ dist/precache-manifest.2f2fd6618fb831ff26bdbc8cce27b21d.js
@@ -0,0 +1,8 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "client.7d34222aa32d4576d6f0.js"
+  },
+  {
+    "url": "vendor.7d34222aa32d4576d6f0.js"
+  }
+]);
