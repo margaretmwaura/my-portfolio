70b115f5f6bad0cf77e355d9709f90d7ea415718
Margaret Mwaura <mwauramargaret1@gmail.com>
2021-05-28 19:58:38 +0300

    adding dist subtree

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.936f09b825e7c5757be5ffac3d00db85.js"
+  "precache-manifest.0d664792253dbc9b3baa0d0564573b47.js"
 );
 
 self.addEventListener('message', (event) => {
