c281fa8545d5062488b5fe8f2fe7835556fef505
Margaret Mwaura <mwauramargaret1@gmail.com>
2021-06-19 21:40:37 +0300

    correcting bio info

--- src/components/about.vue
+++ src/components/about.vue
@@ -4,12 +4,12 @@
 
         <p>
             Hi there 👋 . I am Margaret Mwaura. I currently work as a Software Engineer
-            at Cytonn Investments. I work with Vuejs and Laravel for web applications and Java and Kotlin
-            for android application. I use phpunit to write the backend tests and Mocha and chai to write
+            at Cytonn Investments. I work with Vuejs and Laravel for web applications.
+            I use phpunit to write the backend tests and Mocha and chai to write
             the frontend tests.
         </p>
         <p>
-            I have been working on an android app for an upcoming fin-tech startup in Nairobi , here
+            I have been working on an android app in java for an upcoming fin-tech startup in Nairobi , here
             is the link to the app on play store
             <a href="https://play.google.com/store/apps/details?id=com.patriceandala.verypro">App</a>
         </p>
